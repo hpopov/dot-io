aa47400e3f8c6166cbdf8125d0ee4dc3378f1c8d
John de St. Germain <jdestgermain@users.noreply.github.com>
2024-07-28 09:10:24 -0500

    Update deprecation

--- src/pages/manager/components/ChordManagerHeader.tsx
+++ src/pages/manager/components/ChordManagerHeader.tsx
@@ -8,15 +8,15 @@ export function ChordManagerHeader(): ReactElement {
           Device Manager
         </h1>
       </div>
-      <p>
-        NOTE: This site is deprecated. You should only use it if you are pre-CCOS and are 
-        updating your device to CCOS. Afterwards, you should use the new <a href="https://manager.charachorder.com/config/layout/">Device Manager.</a>
-        See <a href="https://docs.charachorder.com/CCOS.html#upgrade-to-ccos">the documentation</a> for more details on upgrading to CCOS.
-      </p>
       <p className="lg:w-1/2 w-full leading-relaxed text-gray-300 font-semibold font-mono">
         Welcome to the Device Manager. Here you can exercise control over the
         layout and chord library of your chording enabled device.
       </p>
+      <h2 className="lg:w-1/2 w-full leading-relaxed text-gray-300 font-semibold font-mono">
+        NOTE: This site is deprecated. You should only use it if you are pre-CCOS and are 
+        updating your device to CCOS. Afterwards, you should use the new <a className="underline" href="https://manager.charachorder.com/config/layout/">Device Manager.</a>
+        See <a className="underline" href="https://docs.charachorder.com/CCOS.html#upgrade-to-ccos">the documentation</a> for more details on upgrading to CCOS. 
+      </h2>
     </div>
   );
 }
