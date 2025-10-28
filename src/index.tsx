429f3113cccca0cfd3a50541af076daf5713f7c0
Gregory Popov <tordektopor@gmail.com>
2025-10-28 11:53:04 +0000

    Upgrade to React 18 and latest Node (lts/jod)
    Reduced npm audit errors to
    ```
    24 vulnerabilities (1 low, 5 moderate, 18 high)
    ```

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './pages/app/App';
 import './assets/global.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -7,14 +7,13 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // This is the entry point to the React application
 // All pages, components, and functionality lives under this umbrella
 // Start by entering the <App /> component
-ReactDOM.render(
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
-  // window.onbeforeunload = function() {
-  // sessionStorage.clear();
-  //}
 );
 
 serviceWorkerRegistration.register();
